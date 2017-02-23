99e0ec31692ce7791ba5cf9da0440386f49332fe
Daniel <axhammar.daniel@gmail.com>
2017-02-23 10:31:21 +0100

    Added shortcut for installation.

--- Quizdatabase.sql
+++ Quizdatabase.sql
@@ -5,27 +5,26 @@
    source C:\Users\Daniel\Documents\GitHub\MYSQL QUIZ\Quizdatabase.sql
 */
 
-	
-/* */  
-    DROP SCHEMA IF EXISTS QuizDB;
+
+    DROP SCHEMA IF EXISTS quizdb;
 
 	
 /* */
-	CREATE SCHEMA IF NOT EXISTS QuizDB;
-	USE QuizDB;
+	CREATE SCHEMA IF NOT EXISTS quizdb;
+	USE quizdb;
 
 
-	CREATE TABLE USERS (
-	mail int NOT NULL PRIMARY KEY,
-	name varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	groups varchar(255) NOT NULL,
-	accountLevel varchar(255) NOT NULL
+	CREATE TABLE user (
+	mail VARCHAR(100) NOT NULL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	groups VARCHAR(255) NOT NULL,
+	accountLevel VARCHAR(255) NOT NULL
 	);
 	
-	CREATE TABLE Quiz (
-	quizId int NOT NUll AUTO_INCREMENT Primary KEY,
-	questionId int NOT NUll,
+	CREATE TABLE quiz (
+	quizId INT NOT NUll AUTO_INCREMENT Primary KEY,
+	questionId INT NOT NUll,
 	dateCreated TIMESTAMP NOT NULL,
 	dateFinished TIMESTAMP NOT NULL,
 	times TIME,
@@ -33,22 +32,22 @@
 	score SMALLINT NOT NULL
 	);
 	
-	CREATE TABLE Question(
-	questionId int NOT NULL PRIMARY KEY,
-	answerId int NOT NULL,
+	CREATE TABLE question(
+	questionId INT NOT NULL PRIMARY KEY,
+	answerId INT NOT NULL,
 	question LONGTEXT NOT NULL
 	);
 	
-	CREATE TABLE Answers(
+	CREATE TABLE answers(
 	answerId INT NOT NULL PRIMARY KEY,
 	answer TEXT NOT NULL,
 	correct BOOLEAN NOT NULL
 	);
 	
-	CREATE TABLE QuizTanken(
+	CREATE TABLE quiztaken(
 	quizTakenMail VARCHAR(100) NOT NUll Primary KEY,
 	quizTakenId SMALLINT NOT NULL,
-	results VARCHAR(10) NOT NULL
+	results SMALLINT NOT NULL
 	);
 	
 	
