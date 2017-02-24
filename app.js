574a6e84e1822418d4560a3bfcda90fb059a7323
Andreas Ekman <info@andreasekman.com>
2017-02-24 20:08:58 +0100

    Better comments

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 
-app.use(express.static(__dirname + '/public')); // Adding public dir to global
+// Set static folder
+app.use(express.static(__dirname + '/public'));
 
-app.set('view engine', 'ejs'); // Using EJS as template engine
+// View engine
+app.set('view engine', 'ejs');
 
-/* Global variables */
+// Global variables
 app.locals.pageTitle = "Quiz Maker";
 
-/* Routes called via function in routes/index.js*/
+// Routes called via function in routes/index.js
 app.get('/', routes.index);
 app.get('/user', routes.user);
 app.get('/creator', routes.creator);
@@ -20,7 +22,7 @@ app.get('*', function(reg, res) {
     res.send('Page not found 404');
 });
 
-/* Start server on port 3000 */
+// Start server on port 3000
 app.set('port', process.env.PORT || 3000); // use port 3000 unless there exists a preconfigured port
 var server = app.listen(app.get('port'), function() {
     console.log('Listening on port:' +app.get('port'));
