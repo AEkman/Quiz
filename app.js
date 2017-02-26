6486fcdceeee64489e9ed4b1d879ae10fd082ba6
Andreas Ekman <info@andreasekman.com>
2017-02-26 15:49:41 +0100

    Created all pages
    Temporarily gave all admin navigation

--- app.js
+++ app.js
@@ -14,12 +14,64 @@ app.set('view engine', 'ejs');
 app.locals.pageTitle = "Quiz Maker";
 
 // Routes
-/* Home */
+/* Home/Login */
 app.get('/', function(req, res) {
     res.render('index', {
         title: 'Home',
         classname: 'home'
     });
+});
+
+/* Create Quiz */
+app.get('/createquiz', function(req, res) {
+    res.render('createquiz', {
+        title: 'Create Quiz',
+        classname: 'createquiz'
+    });
+});
+
+/* Take Quiz Quiz */
+app.get('/takequiz', function(req, res) {
+    res.render('takequiz', {
+        title: 'Take Quiz',
+        classname: 'takequiz'
+    });
+});
+
+/* Results */
+app.get('/results', function(req, res) {
+    res.render('results', {
+        title: 'Results',
+        classname: 'results'
+    });
+});
+
+/* Profile */
+app.get('/profile', function(req, res) {
+    res.render('profile', {
+        title: 'Profile',
+        classname: 'profile'
+    });
+});
+
+/* Settings */
+app.get('/settings', function(req, res) {
+    res.render('settings', {
+        title: 'Settings',
+        classname: 'settings'
+    });
+});
+
+app.post('/settings', function(req, res) {
+    var user = {
+        mailadress: req.body.mailadress,
+        username: req.body.username,
+        password: req.body.password,
+        group: req.body.group,
+        accountlevel: req.body.accountlevel
+    };
+
+    console.log(user);
 });
 
 /* User */
@@ -45,27 +97,6 @@ app.get('/admin', function(req, res) {
         classname: 'admin'
     });
 });
-
-/* Settings */
-app.get('/settings', function(req, res) {
-    res.render('settings', {
-        title: 'Settings',
-        classname: 'settings'
-    });
-});
-
-app.post('/settings', function(req, res) {
-    var user = {
-        mailadress: req.body.mailadress,
-        username: req.body.username,
-        password: req.body.password,
-        group: req.body.group,
-        accountlevel: req.body.accountlevel
-    };
-
-    console.log(user);
-});
-
 
 // Start server on port 3000
 app.set('port', process.env.PORT || 3000); // use port 3000 unless there exists a preconfigured port
