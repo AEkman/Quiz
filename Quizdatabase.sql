fd313f1457e9df9f8006567b0414bc503a099d92
Daniel <axhammar.daniel@gmail.com>
2017-03-01 21:00:04 +0100

    Merge branch 'master' of C:\Users\Daniel\Documents\GitHub\Quiz with conflicts.

--- Quizdatabase.sql
+++ Quizdatabase.sql
@@ -1,62 +1,65 @@
-DROP SCHEMA IF EXISTS quizdb;
 
 
-CREATE SCHEMA IF NOT EXISTS quizdb;
-USE quizdb;
+    DROP SCHEMA IF EXISTS quizdb;
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-    mail VARCHAR(255) NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    groups VARCHAR(255) NOT NULL,
-    accountLevel VARCHAR(255) NOT NULL
-    );
 
-INSERT INTO user (mail,name, password, groups, accountLevel) VALUES("daniel@com", "daniel", "password","alpha", "creator");
+	CREATE SCHEMA IF NOT EXISTS quizdb;
+	USE quizdb;
 
-DROP TABLE IF EXISTS quiz;
-CREATE TABLE quiz (
-    quizId INT NOT NUll AUTO_INCREMENT PRIMARY KEY,
-    quizName VARCHAR(255) NOT NULL,
-    dateCreated TIMESTAMP NOT NULL,
-    dateFinished DATE NOT NULL,
-    times INT,
-    score SMALLINT NOT NULL
-    );
+    DROP TABLE IF EXISTS user;
+    /* USER IS COMPLETE */
+	CREATE TABLE user (
+	mail VARCHAR(255) NOT NULL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	groups VARCHAR(255) NOT NULL,
+	accountLevel VARCHAR(255) NOT NULL
+	);
+	
+	DROP TABLE IF EXISTS quiz;
+-- quiz is complete
+	CREATE TABLE quiz (
+	quizId INT NOT NUll AUTO_INCREMENT PRIMARY KEY,
+	quizName VARCHAR(255) NOT NULL,
+	dateCreated TIMESTAMP NOT NULL,
+	dateFinished DATE NOT NULL,
+	times INT,
+	score SMALLINT NOT NULL
+	);
+-- question is complete
 
-INSERT INTO quiz (quizName, dateFinished, times, score) VALUES ('ALBAN', '2008-06-13', 25, 20);
+	DROP TABLE IF EXISTS question;
 
-DROP TABLE IF EXISTS question;
-CREATE TABLE question(
-    questionId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    question LONGTEXT NOT NULL,
-    questionQuizid INT NOT NULL,
-    FOREIGN KEY (questionQuizid) REFERENCES quiz(quizId)
-     );
+	CREATE TABLE question(
+	questionId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	question LONGTEXT NOT NULL,
+	questionQuizid INT NOT NULL,
+	FOREIGN KEY (questionQuizid) REFERENCES quiz(quizId)
 
-INSERT INTO question (questionQuizid,question) VALUES (1,"this si shit");
+	);
+-- answers is complete
+	DROP TABLE IF EXISTS answers;
 
-DROP TABLE IF EXISTS answers;
-CREATE TABLE answers(
-    answerId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    answer TEXT NOT NULL,
-    correct BOOLEAN NOT NULL,
-    answerQuestionid INT NOT NULL,
-    FOREIGN KEY (answerQuestionid) REFERENCES question(questionId)
-    );
+	CREATE TABLE answers(
+	answerId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	answer TEXT NOT NULL,
+	correct BOOLEAN NOT NULL,
+	answerQuestionid INT NOT NULL,
+	FOREIGN KEY (answerQuestionid) REFERENCES question(questionId)
+	
+	);
+-- quiztanken is complete
+	DROP TABLE IF EXISTS quiztaken;
 
-INSERT INTO answers (answerQuestionid,correct,answer) VALUES (1,0,"this si shit answer");
+	CREATE TABLE quiztaken(
+	quiztakenId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	quizTakenMail VARCHAR(100) NOT NULL,
+	quizTakenQid INT NOT NULL,
+	results SMALLINT NOT NULL,
+	elapTimes DATETIME,
+	FOREIGN KEY (quizTakenQid) REFERENCES question(questionId),
+	FOREIGN KEY (quizTakenMail) REFERENCES user(mail)
+	);
+	
+	
 
-DROP TABLE IF EXISTS quiztaken;
-    CREATE TABLE quiztaken(
-    quiztakenId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    quizTakenMail VARCHAR(100) NOT NULL,
-    quizTakenQid INT NOT NULL,
-    results SMALLINT NOT NULL,
-    elapTimes DATETIME,
-    FOREIGN KEY (quizTakenQid) REFERENCES question(questionId),
-    FOREIGN KEY (quizTakenMail) REFERENCES user(mail)
-    );
-
-INSERT INTO quizTaken (quizTakenMail, QuizTakenQid, results, elapTimes) VALUES ("daniel@com",1,39,'1000-01-01 00:00:00');
